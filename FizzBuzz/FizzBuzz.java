2a54cd542736208aabcc5a12ea7c4d3f0f4b4f68
jinglepenguin <Elaine.Chu@rci.rogers.ca>
2016-03-11 19:37:03 -0500

    code kata
    initial check in

---
+++ FizzBuzz/FizzBuzz.java
@@ -0,0 +1,26 @@
+package FizzBuzz;
+
+public class FizzBuzz {
+    public static void main (String[] args){
+
+        for(int i = 1; i <= 100; i++){
+            String number = String.valueOf(i);
+            String output = "";
+
+            if( i % 3 == 0 || number.indexOf("3") >= 0){
+                output = "Fizz";
+            }
+
+            if( i % 5 == 0 || number.indexOf("5") >= 0){
+                output = output.concat("Buzz");
+            }
+
+            if(output.length() > 0){
+                System.out.println(output);
+            } else {
+                System.out.println(i);
+            }
+        }
+
+    }
+}
